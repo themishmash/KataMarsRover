1cfcef016df08e1767b1580a7ad8106fab260a48
Michelle <wong.mich.c@gmail.com>
2020-05-06 10:57:26 +1000

    Created Move class and partially wrote test

---
+++ kata-MarsRover/Move.cs
@@ -0,0 +1,10 @@
+namespace kata_MarsRover
+{
+    public class Move
+    {
+        public (int, int) Forward()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
