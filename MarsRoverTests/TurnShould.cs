43f2a633f0b68ca20e9a9636e98deffa1fea5e13
Michelle <wong.mich.c@gmail.com>
2020-05-15 10:48:56 +1000

    Wrote first test for Turn class

---
+++ MarsRoverTests/TurnShould.cs
@@ -0,0 +1,20 @@
+using kata_MarsRover;
+using Xunit;
+
+namespace MarsRoverTests
+{
+    public class TurnShould
+    {
+        [Fact]
+        [Trait("Category", "TurnRight-North")]
+        public void ChangeDirectionToEastIfFacingNorthAndTurningRight()
+        {
+            var sut = new Turn(new Position(0,0 ){Direction = Direction.N});
+            var actualResult = sut.Right();
+            var expectedResult = new Position(0, 0){Direction = Direction.E};
+            Assert.Equal(expectedResult.XCoordinate, actualResult.XCoordinate);
+            Assert.Equal(expectedResult.YCoordinate, actualResult.YCoordinate);
+            Assert.Equal(expectedResult.Direction, actualResult.Direction);
+        }
+    }
+}
