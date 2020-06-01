3dd88613ffcfb8b842ebefad7e3f474573040d6a
Nhan Dang <nhantdang@gmail.com>
2020-06-01 10:54:35 +1000

    added changesd from remote repo

--- kata-MarsRover/InputOutput.cs
+++ kata-MarsRover/InputOutput.cs
@@ -10,13 +10,14 @@ namespace kata_MarsRover
         {
             var elements = initialCoordinates.Split(",", StringSplitOptions.RemoveEmptyEntries);
             var isXCoordinateValid = int.TryParse(elements[0], out var xCoordinate);
-            var isYCoordinateValid = int.TryParse(elements[0], out var YCoordinate);
+            var isYCoordinateValid = int.TryParse(elements[0], out var yCoordinate);
             return isXCoordinateValid && isYCoordinateValid;
         }
         
         public static char[] ParseCommandToArray(string commandString)
         {
             var commandArray = commandString.ToCharArray();
+            //TODO: use select method to convert each char to upper and to toarray
             return commandArray;
         }
         
@@ -28,6 +29,7 @@ namespace kata_MarsRover
         
         public static bool IsDirectionValid(string direction)
         {
+            //TODO: convert direction to upper
             var validDirections = new string[]{"N","E","S","W"};
             return validDirections.Contains(direction);
         }
